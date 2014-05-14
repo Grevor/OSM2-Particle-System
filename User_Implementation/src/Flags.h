fe8a339c25612c718ffbed91ba011d163fa2807b
Andreas Scherman <andreasscherman@gmail.com>
2014-05-14 13:04:25 +0200

    updated more stuff

---
+++ User_Implementation/src/Flags.h
@@ -0,0 +1,35 @@
+#ifndef FLAGS_H_
+#define FLAGS_H_
+
+#define DEBUG 0
+
+#define SPEED_OF_SOUND 340
+#define WATER_DENSITY 1000
+#define PI 3.141516f
+#define E 2.71828f
+#define H 10
+#define HH H*H
+
+#define USE_RADIUS 1
+#define RADIUS_OF_NEIGHBOURS H
+#define AMOUNT_OF_NEIGHBOURS H
+
+#define NUM_WATER_PARTICLES 200
+#define VOLUME 100
+#define NUM_THREADS 10
+
+#define VISCOSITY 5
+
+#define POINT_SIZE 10
+
+#define RAND_DISTRIBUTION 200
+
+#define TIME_STEP 0.05
+
+#define WINDOW_X 640
+#define WINDOW_Y 480
+#define NEAR 320
+#define FAR -320
+#define WHEREVER_YOU_ARE 0
+
+#endif
