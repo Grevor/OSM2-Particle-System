651e01c91c4ce59355446ea27812fce906b7f705
Andreas Scherman <andreasscherman@gmail.com>
2014-05-14 13:00:14 +0200

    updated stuff

--- Concurrent Collections/src/NeighbourTreeTest.cpp
+++ Concurrent Collections/src/NeighbourTreeTest.cpp
@@ -6,18 +6,42 @@
 #include <boost/geometry/index/rtree.hpp>
 #include <boost/foreach.hpp>
 #include <boost/geometry/geometries/point.hpp>
+//#include <boost/test/included/unit_test.hpp>
+#include <assert.h>
+
+#define PARTICLES 100 
+#define NEIGHBOUR_SEARCHES 1
+#define BOOST_TEST_MODULE MyTest
 
 using namespace std;
 
 int main(void) {
     NeighbourTree* tree = new NeighbourTree();
-    for (double i = 0; i < 10; i++) {
-        double pos[] = {i, i};
+    for (double i = 0; i < PARTICLES; i++) {
+        double pos[] = {i, i, i};
         tree->initParticle(pos);
     }
-    double q_p[] = {6, 8};
-    ReturnCarry result = tree->getNeighbours(q_p, 3);
-    tree->printResult(result);
+    double q_p[] = {6, 8, 7};
+    ReturnCarry result;
+    result = tree->getKNeighbours(q_p, 3);
+
+    //ask for 3 neighbours, check we get 3 neighbours
+    assert((tree->getKNeighbours(q_p, 3)->resultIterator).size() == 3);
     
+    q_p[0] = 50;
+    q_p[1] = 50;
+    q_p[2] = 50;
+    
+    assert((tree->getRadiusNeighbours(q_p, 1)->resultIterator).size() == 1);
+
+    double old_pos[] = {1, 1, 1};
+    double new_pos[] = {51, 50, 50};
+    tree->updateParticlePosition(old_pos, new_pos, 0);
+
+    result = tree->getRadiusNeighbours(q_p, 5);
+    tree->printResult(result);
+
+    assert((tree->getRadiusNeighbours(q_p, 1)->resultIterator).size() == 2);
+
     return 0;
 }
--- User_Implementation/src/WaterParticle.h
+++ User_Implementation/src/WaterParticle.h
@@ -1,17 +1,40 @@
 /*
  * WaterParticle.h
  *
- *  Created on: 29 apr 2014
+ *  Created on: 07 maj 2014
  *      Author: andreas 
  */
 
-#ifndef SIMPLEPARTICLE_H_
-#define SIMPLEPARTICLE_H_
+#ifndef WATERPARTICLE_H_ 
+#define WATERPARTICLE_H_ 
 
+#include "../../Particle System/src/ParticleInitializer.h"
+#include "../../Particle System/src/ParticleUpdater.h"
+#include "WaterParticleStruct.h"
+#include "../../Concurrent Collections/src/NeighbourTree.h"
 
-struct waterParticle {
-	int mass, velocity, position, force, density, pressure, color;
-} waterParticle_t;
+class WaterParticle : public ParticleInitializer<struct waterParticle_t>, public ParticleUpdater<struct waterParticle_t> {
+private:
+    NeighbourTree *tree;
+    int particleMass;
 
+    double time_step;
+    double current_time;
 
-#endif /* SIMPLEPARTICLE_H_ */
+public:
+	void initParticle(WaterParticle_t particle) override;
+
+    bool updateParticle(WaterParticle_t particle) override;
+
+    void findAcceleration(WaterParticle_t main, WaterParticle_t neighbour, double distance);
+
+    double getTime(void);
+
+    void RigidBodyCheck(WaterParticle_t particle);
+
+    WaterParticle(int amountOfParticles, int volume, int density, double time_step);
+
+	~WaterParticle() override;
+};
+
+#endif
